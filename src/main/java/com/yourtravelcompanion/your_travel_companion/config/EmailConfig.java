7d14906599cf972b8ff7fec1b21d7758977f6734
Olga <littleshef1306@gmail.com>
2025-04-20 18:16:49 +0300

    change

--- src/main/java/com/yourtravelcompanion/your_travel_companion/config/EmailConfig.java
+++ src/main/java/com/yourtravelcompanion/your_travel_companion/config/EmailConfig.java
@@ -5,7 +5,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.Properties;
 
 @Configuration
 @EnableScheduling
@@ -26,6 +30,23 @@ public class EmailConfig {
 
         return message;
     }
+    @Bean
+    public JavaMailSender javaMailSender(@Value("${spring.mail.username}") String username,
+                                         @Value("${spring.mail.password}") String password) {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
 
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.starttls.required", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
 }
 
