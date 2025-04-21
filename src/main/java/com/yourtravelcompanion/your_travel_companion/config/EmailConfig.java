5e10dd38ea1af75924d8c2c56639c846a9c468cf
Olga <littleshef1306@gmail.com>
2025-04-21 13:27:49 +0300

    change

--- src/main/java/com/yourtravelcompanion/your_travel_companion/config/EmailConfig.java
+++ src/main/java/com/yourtravelcompanion/your_travel_companion/config/EmailConfig.java
@@ -5,17 +5,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
+
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import java.util.Properties;
 
 @Configuration
 @EnableScheduling
 
 public class EmailConfig {
-    @Value("${MAIL_USERNAME}")
+    @Value("olgapracticeprofile@gmail.com")
     private String fromAddress;
 
 
