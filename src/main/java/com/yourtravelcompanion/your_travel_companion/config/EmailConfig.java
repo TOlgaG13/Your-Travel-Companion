225d3177325b5ba43aee5ffdbd36be0006f4a834
Olga <littleshef1306@gmail.com>
2025-04-21 12:49:41 +0300

    change

--- src/main/java/com/yourtravelcompanion/your_travel_companion/config/EmailConfig.java
+++ src/main/java/com/yourtravelcompanion/your_travel_companion/config/EmailConfig.java
@@ -16,7 +16,21 @@ import java.util.Properties;
 
 public class EmailConfig {
 
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
 
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setUsername(System.getenv("MAIL_USERNAME"));
+        mailSender.setPassword(System.getenv("MAIL_PASSWORD"));
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+
+        return mailSender;
+    }
 
 
     @Bean
