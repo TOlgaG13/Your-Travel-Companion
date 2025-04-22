6a4446aaee3a0ccd3ea085167562a5358d1a5029
Olga <littleshef1306@gmail.com>
2025-04-22 21:54:51 +0300

    change

--- src/main/java/com/yourtravelcompanion/your_travel_companion/config/SecurityConfig.java
+++ src/main/java/com/yourtravelcompanion/your_travel_companion/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.yourtravelcompanion.your_travel_companion.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.LockedException;
@@ -10,6 +11,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.web.filter.ForwardedHeaderFilter;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -19,6 +22,12 @@ public class SecurityConfig {
     public SecurityConfig(AuthenticationSuccessHandler authenticationSuccessHandler
                         ) {this.authenticationSuccessHandler = authenticationSuccessHandler;
 
+    }
+    @Bean
+    public FilterRegistrationBean<ForwardedHeaderFilter> forwardedHeaderFilter() {
+        FilterRegistrationBean<ForwardedHeaderFilter> filter = new FilterRegistrationBean<>();
+        filter.setFilter(new ForwardedHeaderFilter());
+        return filter;
     }
 
     @Bean
